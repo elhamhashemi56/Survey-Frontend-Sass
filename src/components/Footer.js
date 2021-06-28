9600bb4582850f2f63739f60042f11276ff90b20
unknown <elhamhashemi56@yahoo.com>
2021-06-28 12:32:40 +0200

    erste push

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <div id={props.id}>
+             <h1>Footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+        <div id={props.id}>
+            <h1>navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
